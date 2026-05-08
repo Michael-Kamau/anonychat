df085b96384d77eaea505bebc05e04472f86b83a
Michael Kamau <kamau.karitu@gmail.com>
2026-05-08 09:57:10 +0300

    - Update rabbitmq connection env

--- config/runtime.exs
+++ config/runtime.exs
@@ -38,6 +38,18 @@ if config_env() == :prod do
     # pool_count: 4,
     socket_options: maybe_ipv6
 
+  rabbitmq_url =
+    System.get_env("RABBITMQ_URL") ||
+      raise """
+      environment variable RABBITMQ_URL is missing.
+      For example: amqp://USER:PASS@HOST:PORT
+      """
+
+  config :anonychat, :amqp,
+    connections: [
+      chat_conn: [url: rabbitmq_url]
+    ]
+
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
   # want to use a different value for prod and you most likely don't want
