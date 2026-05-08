c8beef614bad9ce117d2671328094fed14db49e2
Michael Kamau <kamau.karitu@gmail.com>
2026-05-08 10:39:37 +0300

    update

--- lib/anonychat/amqp/amqp_consumer.ex
+++ lib/anonychat/amqp/amqp_consumer.ex
@@ -2,24 +2,44 @@ defmodule Anonychat.Amqp.AmqpConsumer do
   use GenServer
   use AMQP
 
+  require Logger
+
+  @retry_interval 5_000
+
   def start_link(_opts \\ []) do
     GenServer.start_link(__MODULE__, [], [])
   end
 
-  @exchange    "gen_server_test_exchange"
-  @queue       "gen_server_test_queue"
+  @exchange "gen_server_test_exchange"
+  @queue "gen_server_test_queue"
   @queue_error "#{@queue}_error"
 
   def init(_opts) do
-    {:ok, conn} = Connection.open(amqp_connection_config())
-    {:ok, chan} = Channel.open(conn)
-    setup_queue(chan)
+    {:ok, %{channel: nil}, {:continue, :connect}}
+  end
 
-    # Limit unacknowledged messages to 10
-    :ok = Basic.qos(chan, prefetch_count: 10)
-    # Register the GenServer process as a consumer
-    {:ok, _consumer_tag} = Basic.consume(chan, @queue)
-    {:ok, chan}
+  def handle_continue(:connect, state) do
+    connect(state)
+  end
+
+  def handle_info(:connect, state) do
+    connect(state)
+  end
+
+  defp connect(state) do
+    with {:ok, conn} <- Connection.open(amqp_connection_config()),
+         {:ok, chan} <- Channel.open(conn),
+         :ok <- setup_queue(chan),
+         :ok <- Basic.qos(chan, prefetch_count: 10),
+         {:ok, _consumer_tag} <- Basic.consume(chan, @queue) do
+      Logger.info("AMQP consumer connected")
+      {:noreply, %{state | channel: chan}}
+    else
+      {:error, reason} ->
+        Logger.warning("AMQP consumer connection failed: #{inspect(reason)}")
+        schedule_reconnect()
+        {:noreply, %{state | channel: nil}}
+    end
   end
 
   defp amqp_connection_config do
@@ -37,52 +57,60 @@ defmodule Anonychat.Amqp.AmqpConsumer do
   end
 
   # Confirmation sent by the broker after registering this process as a consumer
-  def handle_info({:basic_consume_ok, %{consumer_tag: _consumer_tag}}, chan) do
-    {:noreply, chan}
+  def handle_info({:basic_consume_ok, %{consumer_tag: _consumer_tag}}, state) do
+    {:noreply, state}
   end
 
   # Sent by the broker when the consumer is unexpectedly cancelled (such as after a queue deletion)
-  def handle_info({:basic_cancel, %{consumer_tag: _consumer_tag}}, chan) do
-    {:stop, :normal, chan}
+  def handle_info({:basic_cancel, %{consumer_tag: _consumer_tag}}, state) do
+    schedule_reconnect()
+    {:noreply, %{state | channel: nil}}
   end
 
   # Confirmation sent by the broker to the consumer process after a Basic.cancel
-  def handle_info({:basic_cancel_ok, %{consumer_tag: _consumer_tag}}, chan) do
-    {:noreply, chan}
+  def handle_info({:basic_cancel_ok, %{consumer_tag: _consumer_tag}}, state) do
+    {:noreply, state}
   end
 
-  def handle_info({:basic_deliver, payload, %{delivery_tag: tag, redelivered: redelivered}}, chan) do
+  def handle_info(
+        {:basic_deliver, payload, %{delivery_tag: tag, redelivered: redelivered}},
+        %{channel: chan} = state
+      ) do
     # You might want to run payload consumption in separate Tasks in production
     consume(chan, tag, redelivered, payload)
-    {:noreply, chan}
+    {:noreply, state}
   end
 
   defp setup_queue(chan) do
     {:ok, _} = Queue.declare(chan, @queue_error, durable: true)
+
     # Messages that cannot be delivered to any consumer in the main queue will be routed to the error queue
-    {:ok, _} = Queue.declare(chan, @queue,
-                             durable: true,
-                             arguments: [
-                               {"x-dead-letter-exchange", :longstr, ""},
-                               {"x-dead-letter-routing-key", :longstr, @queue_error}
-                             ]
-                            )
+    {:ok, _} =
+      Queue.declare(chan, @queue,
+        durable: true,
+        arguments: [
+          {"x-dead-letter-exchange", :longstr, ""},
+          {"x-dead-letter-routing-key", :longstr, @queue_error}
+        ]
+      )
+
     :ok = Exchange.fanout(chan, @exchange, durable: true)
     :ok = Queue.bind(chan, @queue, @exchange)
   end
 
   defp consume(channel, tag, redelivered, payload) do
     binary_string = payload |> to_string() |> String.trim()
-    if is_binary(binary_string) do
-      :ok = Basic.ack channel, tag
 
-        AnonychatWeb.Endpoint.broadcast(
+    if is_binary(binary_string) do
+      :ok = Basic.ack(channel, tag)
+
+      AnonychatWeb.Endpoint.broadcast(
         "amqp:user_messages",
         "new_message",
         %{body: binary_string, created_at: DateTime.utc_now() |> DateTime.to_iso8601()}
       )
     else
-      :ok = Basic.reject channel, tag, requeue: false
+      :ok = Basic.reject(channel, tag, requeue: false)
       IO.puts "#{binary_string} is not a valid binary string and was rejected."
     end
 
@@ -95,7 +123,11 @@ defmodule Anonychat.Amqp.AmqpConsumer do
     # Make sure you call ack, nack or reject otherwise consumer will stop
     # receiving messages.
     _exception ->
-      :ok = Basic.reject channel, tag, requeue: not redelivered
+      :ok = Basic.reject(channel, tag, requeue: not redelivered)
       IO.puts "Error converting #{payload} to integer"
+  end
+
+  defp schedule_reconnect do
+    Process.send_after(self(), :connect, @retry_interval)
   end
 end
